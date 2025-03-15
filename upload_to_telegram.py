279ed94669a1a7407752ec4999fc4089c7b25add
Aliza <alirezanajmabadi79@gmail.com>
2025-03-15 22:41:27 +0330

    Update upload_to_telegram.py

--- upload_to_telegram.py
+++ upload_to_telegram.py
@@ -1,12 +1,14 @@
 import os
 import sys
+import asyncio
 from telethon import TelegramClient
 from telethon.errors import RPCError
 
 # Function to upload file using Telethon
 async def upload_file(file_path, bot_token, chat_id):
     # Initialize the Telegram client
-    client = TelegramClient('bot_session', api_id="2040", api_hash="b18441a1ff607e10a989891a5462e627").start(bot_token=bot_token)
+    client = TelegramClient('bot_session', api_id="2040", api_hash="b18441a1ff607e10a989891a5462e627")
+    await client.start(bot_token=bot_token)
 
     try:
         # Upload the file
@@ -24,5 +26,4 @@ if __name__ == "__main__":
     chat_id = sys.argv[3]
 
     # Run the async function
-    import asyncio
     asyncio.run(upload_file(file_path, bot_token, chat_id))
